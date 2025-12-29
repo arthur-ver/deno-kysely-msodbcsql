b79937899d0c681d6b87381dd0a805522115f7ea
Arthur <19251998+arthur-ver@users.noreply.github.com>
2025-12-29 16:27:32 +0100

    Update driver.ts

--- driver.ts
+++ driver.ts
@@ -1,19 +1,28 @@
 import { Pool } from "tarn";
+import type { PoolOptions } from "tarn/dist/Pool";
+
 import {
   DatabaseConnection,
   Driver,
   TransactionSettings,
+  MssqlDialectConfig,
 } from "@kysely/kysely";
 import { odbcLib, allocHandle, SQL_HANDLE_ENV } from "./ffi.ts";
-
 import { OdbcConnection } from "./connection.ts";
 
-export interface OdbcDialectConfig {
-  connectionString: string;
-  pool?: {
-    min: number;
-    max: number;
+export interface OdbcDialectConfig
+  extends Omit<MssqlDialectConfig, "tedious" | "tarn"> {
+  odbc: Odbc;
+  tarn: {
+    options: Omit<
+      PoolOptions<OdbcConnection>,
+      "create" | "destroy" | "validate"
+    >;
   };
+}
+
+export interface Odbc {
+  connectionString: string;
 }
 
 export class OdbcDriver implements Driver {
@@ -26,14 +35,13 @@ export class OdbcDriver implements Driver {
     this.#config = Object.freeze({ ...config });
 
     this.#pool = new Pool({
-      min: this.#config.pool?.min ?? 0,
-      max: this.#config.pool?.max ?? 10,
+      ...this.#config.tarn.options,
       create: async () => {
         if (this.#envHandle === null) {
           throw new Error("Driver not initialized: envHandle is missing");
         }
         const connection = new OdbcConnection(
-          this.#config.connectionString,
+          this.#config.odbc.connectionString,
           this.#envHandle
         );
         await connection.connect();
@@ -42,7 +50,7 @@ export class OdbcDriver implements Driver {
       destroy: async (connection) => {
         await connection.destroy();
       },
-      validate: undefined,
+      validate: undefined, // TODO: https://github.com/kysely-org/kysely/blob/master/src/dialect/mssql/mssql-driver.ts#L63
     });
   }
 
@@ -69,7 +77,18 @@ export class OdbcDriver implements Driver {
     await connection.rollbackTransaction();
   }
 
+  // TODO: async savepoint()
+  // TODO: async rollbackToSavepoint()
+
   async releaseConnection(connection: OdbcConnection): Promise<void> {
+    if (
+      this.#config.resetConnectionsOnRelease
+      // NOTE: deprecated code removed
+      // || this.#config.tedious.resetConnectionOnRelease
+    ) {
+      await connection.reset();
+    }
+
     this.#pool.release(connection);
   }
 
